67081f44314e28ed75b2e1a4feb5bc30950f8d8a
David Golden <xdg@xdg.me>
2016-06-21 17:56:35 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Perl/OSType.pm
+++ lib/Perl/OSType.pm
@@ -4,7 +4,7 @@ use warnings;
 package Perl::OSType;
 # ABSTRACT: Map Perl operating system names to generic types
 
-our $VERSION = '1.010';
+our $VERSION = '1.011';
 
 require Exporter;
 our @ISA = qw(Exporter);
